61155ad276b8d56dda17365100d543a22ff0dc5a
Student_EN <ul0280477@edu.uni.lodz.pl>
2024-10-17 13:46:30 +0200

    More changes

--- app/src/main/java/com/example/gitexampleg5/MainActivity.java
+++ app/src/main/java/com/example/gitexampleg5/MainActivity.java
@@ -11,5 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //Those are some of the first changes
+
+        //More changes
     }
 }
